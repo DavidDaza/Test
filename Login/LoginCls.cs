c6112628968e88760dde8534fbd398fbbe71a9bf
PC-HOGAR\David <arcasd484@gmail.com>
2018-10-07 16:00:59 -0500

    Upload Two

--- Login/LoginCls.cs
+++ Login/LoginCls.cs
@@ -1,1 +1,15 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LoginTest
+{
+   public  class LoginCls
+    {
+        public String autenticacionDeUsuarios(string p1, string p2)
+        {
+            return "Autenticacion de usuario correcta.";
+        }
+    }
+}
