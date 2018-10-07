b5f5045fe2a4f53ea6a81e999f68d1e442e63dcf
PC-HOGAR\David <arcasd484@gmail.com>
2018-10-07 15:38:31 -0500

    Prueba de concepto Upload

---
+++ LoginTest/LoginTestCls.cs
@@ -0,0 +1,20 @@
+﻿
+using NUnit.Framework;
+using Login;
+
+namespace LoginTest
+{
+    [TestFixture]
+    public class LoginTestCls
+    {
+        [TestCase]
+        public void Autenticacion_de_usuario_correcta()
+        {
+            var login = new LoginCls();
+
+            Assert.AreEqual("Autenticacion de usuario correcta.", login.autenticacionDeUsuarios("uswe","Pass1258/*"));
+
+        }
+
+    }
+}
